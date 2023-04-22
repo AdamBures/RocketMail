49ee4f298c0760ca2543ab70e8a20219ec6a4430
AdamBures <47189693+AdamBures@users.noreply.github.com>
2023-04-22 20:02:05 +0200

    Update models.py

--- RocketMail/main/models.py
+++ RocketMail/main/models.py
@@ -1,8 +1,14 @@
 from django.db import models
-from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.core.validators import MinLengthValidator, MaxLengthValidator, FileExtensionValidator
 
 # Create your models here.
 class User(models.Model):
 	username = models.CharField(max_length=30)
 	password = models.CharField(max_length=15, validators=[MinLengthValidator(8), MaxLengthValidator(15)])
 	created_at = models.DateTimeField(auto_now_add=True)
+
+class Email(models.Model):
+	sender = models.CharField(max_length=30)
+	recipient = models.CharField(max_length=30)
+	content = models.CharField(max_length=30)
+	IPFS = models.FileField(upload_to="IPFS/", validators=[FileExtensionValidator(allowed_extensions=["pdf", "rtf", "txt"])])
